5737fbb523bb8f40ecfcbf27e7caf54870185575
anilkumarpasvan <46481484+anilkumarpasvan@users.noreply.github.com>
2019-07-12 23:10:50 +0530

    Update vovelconsonants.py

--- vovelconsonants.py
+++ vovelconsonants.py
@@ -1,7 +1,10 @@
-n=input()
-if(n=='a' or n=='e' or n=='i' or n=='u' or n=='o'):
-  print("vovel")
-elif(n!='a' or n!='e' or n!='i' or n!='u' or n!='o'):
-  print("consonent")
+x1=input()
+if((x1>='a' and x1<='z') or (x1>='A' and x1<='Z')):
+  x1=x1.lower()
+  if(x1=='a' or x1=='e' or x1=='i' or x1=='o' or x1=='u'):
+     print("Vowel")
+  else:
+     print("Consonant")
 else:
-  print("invalid")
+    print("invalid")
+    
