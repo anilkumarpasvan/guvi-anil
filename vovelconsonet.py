828f5a38ae2a65dea5d2611844ccfd5e5a5bbcb3
anilkumarpasvan <46481484+anilkumarpasvan@users.noreply.github.com>
2019-07-12 22:59:14 +0530

    Add files via upload

---
+++ vovelconsonet.py
@@ -0,0 +1,7 @@
+n=input()
+if(n=='a' or n=='e' or n=='i' or n=='u' or n=='o'):
+  print("vovel")
+elif(n!='a' or n!='e' or n!='i' or n!='u' or n!='o'):
+  print("consonent")
+else:
+  print("invalid")
