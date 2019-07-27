ef613569e316ae6246ae2bafd050ca20e9c364dd
anilkumarpasvan <46481484+anilkumarpasvan@users.noreply.github.com>
2019-07-27 10:07:49 +0530

    Update largeno.py

--- largeno.py
+++ largeno.py
@@ -1,6 +1,6 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-num3 = int(input("Enter third number: "))
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
  
 if (num1 > num2) and (num1 > num3):
    largest = num1
@@ -8,4 +8,4 @@ elif (num2 > num1) and (num2 > num3):
    largest = num2
 else:
    largest = num3
-print("The largest number is",largest)
+print(largest)
